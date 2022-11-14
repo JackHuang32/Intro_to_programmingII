3360c43739ffb0582538143732fd7da99bffa0f3
JackHuang32 <jack900707@gmail.com>
2022-11-14 13:24:09 +0800

    add cpp

--- hulks_trouble/hulks_trouble.cpp
+++ hulks_trouble/hulks_trouble.cpp
@@ -1,19 +1,41 @@
 #include<iostream>
+#include<algorithm>
 #include<vector>
+#include<map>
 using namespace std;
 int main()
 {
     int n;
     cin >> n;
-    vector<int>arr(n);
+    map<int,int>hash;
     for(int i=0;i<n;i++)
     {
-        cin >> arr[i];
+        int tmp;
+        cin >> tmp;
+        if(hash.find(tmp) != hash.end())
+        {
+            hash[tmp]++;
+        }
+        else
+        {
+            hash[tmp] = 1;
+        }
     }
-    for(auto n:arr)
+    int q;
+    cin >> q;
+    while(q--)
     {
-        cout<<n<<" ";
+        int num;
+        cin >> num;
+        if(hash.find(num) != hash.end())
+        {
+            cout<<hash.find(num)->second<<endl;
+        }
+        else
+        {
+            cout<<"0"<<endl;
+        }
     }
-    cout<<endl;
+
     return 0;
 }

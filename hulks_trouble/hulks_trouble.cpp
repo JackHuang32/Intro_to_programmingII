e4e843975b5c156d7554f0dbb443305af2679243
JackHuang32 <jack900707@gmail.com>
2022-11-14 13:23:27 +0800

    add cpp

---
+++ hulks_trouble/hulks_trouble.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    for(auto n:arr)
+    {
+        cout<<n<<" ";
+    }
+    cout<<endl;
+    return 0;
+}

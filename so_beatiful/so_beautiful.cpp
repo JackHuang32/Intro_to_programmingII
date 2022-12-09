b69d4b94654159c2b40a5bfc00a96a0d359a71ab
JackHuang32 <jack900707@gmail.com>
2022-12-09 23:15:45 +0800

    add files

---
+++ so_beatiful/so_beautiful.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<map>
+#include<utility>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    map<pair<char,int>,int>Second;
+    map<int,int>first;
+    int second_num=0;
+    int first_num=0;
+    while(n--)
+    {
+        int vow_num = 0;
+        char first_vow = '0';
+        string word;
+        cin>>word;
+        for(auto chr:word)
+        {
+            if(chr == 'a'||chr=='e'||chr=='i'||chr=='o'||chr=='u')
+            {
+                vow_num++;
+                if(vow_num==1)
+                {
+                    first_vow = chr;
+                }
+            }
+        }
+        if(vow_num==0)continue;
+        else
+        {
+            if(Second.find(make_pair(first_vow,vow_num))!=Second.end())
+            {
+                int num = ++Second[make_pair(first_vow,vow_num)];
+                if(num%2==0)
+                {
+                    second_num++;
+                }
+            }
+            else
+            {
+                Second[make_pair(first_vow,vow_num)]=1;
+            }
+            if(first.find(vow_num)!=first.end())
+            {
+                first[vow_num]++;
+                if(first[vow_num]%2==0)
+                {
+                    first_num++;
+                }
+            }
+            else
+            {
+                first[vow_num]=1;
+            }
+        }
+        if(second_num >= first_num/2)
+        {
+            cout<<first_num/2<<endl;
+        }
+        else
+        {
+            cout<<second_num<<endl;
+        }
+    }
+    return 0;
+}
